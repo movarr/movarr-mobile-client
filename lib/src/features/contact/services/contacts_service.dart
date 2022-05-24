137c4d9dbd221838a98ea7832f026afd8807d22c
Harsh Kumar <harshkumar@Harshs-MacBook-Air.local>
2022-05-24 16:29:39 +0530

    create contact service to talk with flutter contacts

---
+++ lib/src/features/contact/services/contacts_service.dart
@@ -0,0 +1,16 @@
+import 'dart:js';
+
+import 'package:flutter_contacts/flutter_contacts.dart';
+
+class ContactsService {
+  Future<int> getContactsCount() async {
+    final contacts = await FlutterContacts.getContacts();
+    return contacts.length;
+  }
+
+  Future<bool> requestContactsPermission() async =>
+      await FlutterContacts.requestPermission();
+
+  Future<List<Contact>> getContacts() async =>
+      await FlutterContacts.getContacts();
+}
