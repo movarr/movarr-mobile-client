901d1d13d4fcd605a5edd392ce79d2429c733cc5
Harsh Kumar <harshkumar@Harshs-MacBook-Air.local>
2022-05-25 18:59:37 +0530

    add backup method in app repository interface

--- lib/src/features/apps/domain/apps_repository.dart
+++ lib/src/features/apps/domain/apps_repository.dart
@@ -2,4 +2,5 @@ import '../entities/application_entity.dart';
 
 abstract class AppsRepository {
   Future<List<ApplicationEntity>> getInstalledApps();
+  Future<void> backupInstalledApps(List<ApplicationEntity> apps);
 }
