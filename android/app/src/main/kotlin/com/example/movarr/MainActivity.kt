1a88b6d2164046932403495da9b4b3ff49200e37
Harsh Kumar <harshkumar@Harshs-MacBook-Air.local>
2022-06-02 16:49:13 +0530

    update xml to show application as sms app

--- android/app/src/main/kotlin/com/example/movarr/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/movarr/MainActivity.kt
@@ -1,6 +1,32 @@
 package com.example.movarr
 
+import android.content.Intent
+import android.provider.Telephony
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "smsChannel"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "setDefaultSms") {
+                try {
+                    val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+                    intent.putExtra(
+                        Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
+                        "com.example.movarr"
+                    );
+                    startActivity(intent);
+                    result.success("success")
+                } catch (ex: Exception) {
+                    result.error("UNAVAILABLE", "Setting default sms.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
